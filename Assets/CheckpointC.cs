d57115c9d274f7a5470809fe9089002fa52650cf
sjnasr <sjnasr18@yahoo.com>
2016-08-01 14:38:55 -0700

    Added comments to checkpointC.cs

--- Assets/CheckpointC.cs
+++ Assets/CheckpointC.cs
@@ -4,23 +4,26 @@ using UnityEngine.Advertisements;
 using System.Collections;
 
 public class CheckpointC : MonoBehaviour {
+	//initialized all the variables to 0.
     float setTime = 0.0f;
     double lapTime = 0;
     double bestTime = 0;
     public Text writeScore;
 
-    // Use this for initialization
+    // Use this for initialization start time
     void Start () {
         setTime = 0.0f;
     }
 	
-	// Update is called once per frame
+	// When the player reach the checkpoint for the first time, it will be update the best time
 	void Update () {
         setTime += Time.deltaTime;
-        lapTime = Mathf.Round(setTime);
+        lapTime = Mathf.Round(setTime); //rounds the seconds to a whole number otherwise it will show milleseconds
         writeScore.text = "Time: " + lapTime + "\n Best Time: " + bestTime;
     }
 
+	//Everytime the user hit the checkpoint, laptime will be check to see if it is over 5 seconds, and if the laptime is 
+	//less than best time then it will update best time then ShowAd() is triggered whenever the user reach the checkpoint
     void OnTriggerEnter(Collider other ){
         if (lapTime > 5){
             Debug.Log("" + lapTime);
